064cc8b745ff2f82b42bd8492b823c378ff10718
ViniciusFurlanPolo <vinimurta4740@gmail.com>
2020-09-07 14:56:47 -0300

    Arquivos Dto,Response e Verifid

--- src/main/java/com/cartoes/api/repositories/TransacaoRepository.java
+++ src/main/java/com/cartoes/api/repositories/TransacaoRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import com.cartoes.api.entities.Transacao;
-
+	
 @Transactional(readOnly = true)
 public interface TransacaoRepository extends JpaRepository <Transacao, Integer>{
 	
